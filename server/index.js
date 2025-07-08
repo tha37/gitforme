3fda15f8d9affeecfdc02153a92849aa957f94bf
herin <herinsoni3737@gmail.com>
2025-07-08 16:40:47 +0530

    Auth Updated

---
+++ server/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const authRoute = require("./Routes/AuthRoute");
+const app = express();
+require("dotenv").config({ debug: true })
+const {MONGO_URL } = process.env;
+const PORT = 3000;
+mongoose
+    .connect(MONGO_URL,{})
+  .then(() => console.log("MongoDB is  connected successfully"))
+  .catch((err) => console.error(err));
+
+
+app.use(
+    cors({
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+    })
+)
+app.use(express.json());
+app.use("/", authRoute);
+
+app.listen(PORT,()=>{
+    console.log( `Server is listening on ${PORT}`);
+});
