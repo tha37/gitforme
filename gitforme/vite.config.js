e1911274c9ed1557c5622e948cdaf8cf2d377c31
Herin Soni <herinsoni3737@gmail.com>
2025-07-17 22:57:36 +0530

    react-hot-toast added to viteconfig

--- gitforme/vite.config.js
+++ gitforme/vite.config.js
@@ -5,6 +5,14 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ['react-hot-toast']
+  },
+  build: {
+    commonjsOptions: {
+      include: [/react-hot-toast/, /node_modules/]
+    }
+  },
   resolve: {
     alias: {
       // Example of where 'path' would be used
