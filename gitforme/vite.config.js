1204b83d6f86b4be9ed4dd7f48b3f52863a5ea78
Herin Soni <herinsoni3737@gmail.com>
2025-07-17 11:37:25 +0530

    Trigger workflow

--- gitforme/vite.config.js
+++ gitforme/vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// vite.config.js
 export default defineConfig({
-  plugins: [react()],
+  resolve: {
+    extensions: ['.js', '.jsx', '.json', '.mjs'],
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  }
 })
