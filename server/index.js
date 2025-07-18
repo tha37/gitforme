80dd151de8922afedbb89229551b0b043be1960a
Herin Soni <herinsoni3737@gmail.com>
2025-07-19 01:21:06 +0530

    FIX: Force update server port for Render

--- server/index.js
+++ server/index.js
@@ -20,7 +20,8 @@ const allowedOrigins = [
   'https://www.gitforme.tech',
   'https://thankful-dune-02c682800.2.azurestaticapps.net',
   'https://gitforme-bot.onrender.com',
-  'https://gitforme.onrender.com'
+  'https://gitforme.onrender.com',
+  'https://gitforme-jbsp.vercel.app'
 ];
 app.use(cors({
   origin: function (origin, callback) {
