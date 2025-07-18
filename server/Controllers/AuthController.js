2c4c1d2e0be0498a8629a08b76cd47b7156361e5
Herin Soni <herinsoni3737@gmail.com>
2025-07-19 02:43:20 +0530

    Update AuthController.js

--- server/Controllers/AuthController.js
+++ server/Controllers/AuthController.js
@@ -46,11 +46,11 @@ exports.githubCallback = async (req, res) => {
         req.session.userId = user._id;
 
         // Redirect back to the frontend
-        res.redirect('https://gitforme-jbsp.vercel.app/');
+        res.redirect('https://www.gitforme.tech/');
 
     } catch (error) {
         console.error('Error during GitHub authentication:', error.message);
-        res.redirect('https://gitforme-jbsp.vercel.app/login?error=auth_failed');
+        res.redirect('https://www.gitforme.tech/login?error=auth_failed');
     }
 };
 
