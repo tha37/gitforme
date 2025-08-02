8100b28c43f39ccaa6e2bd39136a41952640def2
Herin Soni <herinsoni3737@gmail.com>
2025-08-02 22:13:07 +0530

    Adsense update

--- gitforme/src/main.jsx
+++ gitforme/src/main.jsx
@@ -1,20 +1,28 @@
-import {ReactDOM, StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import {BrowserRouter} from 'react-router-dom';
-import 'react-toastify/dist/ReactToastify.css';
+import React, { StrictMode } from 'react'; 
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import { CookiesProvider } from 'react-cookie';
 import { AuthProvider } from './context/AuthContext.jsx';
+import App from './App.jsx';
+import './index.css';
+import 'react-toastify/dist/ReactToastify.css';
+const adsenseId = import.meta.env.VITE_ADSENSE_PUB_ID;
+if (adsenseId) {
+  const script = document.createElement('script');
+  script.async = true;
+  script.src = `https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${adsenseId}`;
+  script.crossOrigin = 'anonymous';
+  document.head.appendChild(script);
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <CookiesProvider>
-    <BrowserRouter>
-      <AuthProvider> 
-        <App />
-      </AuthProvider>
-    </BrowserRouter>
+      <BrowserRouter>
+        <AuthProvider> 
+          <App />
+        </AuthProvider>
+      </BrowserRouter>
     </CookiesProvider>
   </StrictMode>,
-)
+);
